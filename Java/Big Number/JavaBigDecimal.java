e9e9e7b1974aa3e4cfde4d1c7f337830476dd9df
Sumit Gupta <41279402+sumitgupta1312@users.noreply.github.com>
2020-11-24 15:21:27 +0530

    Update JavaBigDecimal.java

--- Java/Big Number/JavaBigDecimal.java
+++ Java/Big Number/JavaBigDecimal.java
@@ -50,7 +50,7 @@ import java.util.*;
 
 class JavaBigDecimal{
 
-    public static void main(String []argh)
+    public static void main(String []args)
     {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();

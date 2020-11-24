50e61fa4b374208f4493602e9e4574683bb159fe
Sumit Gupta <41279402+sumitgupta1312@users.noreply.github.com>
2020-11-24 12:10:13 +0530

    Update JavaDeque.java
    adding Deque and ArrayDeque Package

--- Java/Collections/JavaDeque.java
+++ Java/Collections/JavaDeque.java
@@ -44,30 +44,35 @@ Sample Output
 
 */
 
-import java.util.*;
-public class test {
-   public static void main(String[] args) {
-            
-       Scanner in = new Scanner(System.in);
-       Deque deque = new ArrayDeque<Integer>();
-       int n = in.nextInt();
-       int m = in.nextInt();
-       int maxUnique = 0;
-       for (int i = 0; i < n; i++) {
-            int num = in.nextInt();
-            if(i == 0){
-                deque.add(num);  
-                maxUnique++;
-            }else{
-                if(deque.size() == m){
-                    deque.removeFirst();
-                }
-                if(!deque.contains(num) && maxUnique<m){
-                    maxUnique++;
-                }
-                deque.addLast(num);
-            }
-       }
-       System.out.println(""+maxUnique);
-    }
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Test
+{
+	 public static void main(String[] args) {
+         
+	       Scanner in = new Scanner(System.in);
+	       Deque deque = new ArrayDeque<Integer>();
+	       int n = in.nextInt();
+	       int m = in.nextInt();
+	       int maxUnique = 0;
+	       for (int i = 0; i < n; i++) {
+	            int num = in.nextInt();
+	            if(i == 0){
+	                deque.add(num);  
+	                maxUnique++;
+	            }else{
+	                if(deque.size() == m){
+	                    deque.removeFirst();
+	                }
+	                if(!deque.contains(num) && maxUnique<m){
+	                    maxUnique++;
+	                }
+	                deque.addLast(num);
+	            }
+	       }
+	       System.out.println(""+maxUnique);
+	    }
+
 }

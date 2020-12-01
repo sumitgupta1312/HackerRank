ad65ff3a9989142cc9a0bd15ec54762ac4df43fb
Sumit Gupta <41279402+sumitgupta1312@users.noreply.github.com>
2020-12-01 14:03:29 +0530

    Update JavaInstanceOf.java

--- Java/Object Oriented Programming/JavaInstanceOf.java
+++ Java/Object Oriented Programming/JavaInstanceOf.java
@@ -32,13 +32,11 @@ class Rockstar{   }
 class Hacker{}
 
 
-public class JavaInstanceOf
-{
-   static String count(ArrayList mylist)
-   {
-      int a=0,b=0,c=0;
-      for(int i=0;i<mylist.size();i++)
-      {
+public class InstanceOFTutorial{
+	
+   static String count(ArrayList mylist){
+      int a = 0,b = 0,c = 0;
+      for(int i = 0; i < mylist.size(); i++){
          Object element=mylist.get(i);
          if(element instanceof Student)
             a++;
@@ -47,17 +45,15 @@ public class JavaInstanceOf
          if(element instanceof Hacker)
             c++;
       }
-      String ret= Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
+      String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
       return ret;
    }
 
-   public static void main(String []argh)
-   {
-      ArrayList mylist=new ArrayList();
-      Scanner sc=new Scanner(System.in);
-      int t=sc.nextInt();
-      for(int i=0;i<t;i++)
-      {
+   public static void main(String []args){
+      ArrayList mylist = new ArrayList();
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      for(int i=0; i<t; i++){
          String s=sc.next();
          if(s.equals("Student"))mylist.add(new Student());
          if(s.equals("Rockstar"))mylist.add(new Rockstar());
